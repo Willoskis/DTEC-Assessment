db4d7c021f02c99b20092372ca7b10325da93ec2
Glitch (will-h-dtec-assesment) <none>
2020-06-03 03:14:28 +0000

    Index Framework Complete

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ function closeNav() {
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -27,3 +29,9 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+// Loading Screen
+$(window).on("load", function() {
+  $(".loader-wrapper").fadeOut("slow");
+});
